17efdbd73f4f63b0b30bd702ca033bbf0cfb5d10
harsha <ande.sriharsha@gmail.com>
2017-07-16 17:52:24 -0500

    hello

---
+++ Eureka/src/main/java/snippet/Snippet.java
@@ -0,0 +1,6 @@
+package snippet;
+
+public class Snippet {
+	The Tomcat connector configured to listen on port 8443 failed to start. The port may already be in use or the connector may be misconfigured.
+}
+
